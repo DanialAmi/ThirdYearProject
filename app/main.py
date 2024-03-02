31e768d98c9826b905496f7c62552851c42d67d5
Danial Ami <danialami@Danials-MBP.cust.communityfibre.co.uk>
2024-03-02 00:57:31 +0000

    connected the backend to the frontend and now have a fully working website using tfidf as the document retrieval

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
-from data_processing.document_retrieval import preprocess_text
-
+from data_processing.document_retrieval import preprocess_text, load_data_and_vectorizer, process_query
+from data_processing.machine_comprehension import get_best_answer, get_one_best_answer
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -14,7 +14,11 @@ async def read_form():
 @app.post("/query/")
 async def handle_query(query: str = Form(...)):
     # Placeholder for processing the query
-    processed_tokens = preprocess_text(query)
-    processed_text = ' '.join(processed_tokens)
+    df, vectorizer, tfidf_matrix = load_data_and_vectorizer()
+    cosine_similarities = process_query(query, vectorizer, tfidf_matrix)
+    most_similar_doc_index = cosine_similarities.argmax()
+    most_similar_document = df.iloc[most_similar_doc_index]['original_document']
+    print("document found")
+    answer = get_one_best_answer(query, most_similar_document)
  
-    return JSONResponse(content={"processed_query": processed_text})
+    return JSONResponse(content={"processed_query":answer['answer']})
--- data_processing/machine_comprehension.py
+++ data_processing/machine_comprehension.py
@@ -9,3 +9,7 @@ def get_best_answer(query, documents):
         answers.append((result['answer'], result['score']))
     best_answer = sorted(answers, key=lambda x: x[1], reverse=True)[0]
     return best_answer
+
+def get_one_best_answer(query, document):
+    result = qa_pipeline(question = query, context=document)
+    return result
